a641f7b9e2a154c70a9ae80b6f9b0372a0762499
hello <hello@ifnot.cc>
2017-03-28 17:34:09 +0800

    base

---
+++ hello/base.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+a = 'aaa'
+b = "bbb"
+c = """ccc"""
+
+abc = a + \
+    b + \
+    c
+
+ddd = ['aa', "bb", """cc""", abc]
+
+eee = {'aa', "bb", """cc""", abc}
+
+fff = """其实我之后又一个换行符
+我tm真是帅！
+其实我之前又一个换行符"""
+
+"""
+这tm是注释！
+多行注释！！！
+"""
+
+# print(abc)
+# print(ddd)
+# print(eee)
+# print(fff)
+
+### input
+
+# aa = input("aa")
+# print(aa)
+
+# import sys; zxc = "input: #" + aa + "#"; sys.stdout.write(zxc)
