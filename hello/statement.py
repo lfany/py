8e0d1a5fcb981674334235176b8aa09cb16fa9c7
hello <hello@ifnot.cc>
2017-03-29 10:45:02 +0800

    code

--- hello/statement.py
+++ hello/statement.py
@@ -1,26 +1,33 @@
 #!/usr/bin/env python3
 
-if not True:
+if not True and True:
     print("hello")
-else:
+elif not True or False :
     print("world")
+elif False and True:
+    print("=====")
+else:
+    print("=====")
 
 count = 0
 while count < 9:
     count += 1
-    print(count)
+    # print(count)
+    pass
 
 
 a = {'a', 'b', 'c'}
 
 for item in a:
-    print(item)
+    # print(item)
+    pass
 
 import math
 for i in range(0, 100):
-    print(i)
+    # print(i)
+    pass
 
-print(math.pi)
+# print(math.pi)
 
 # import random
 # print(random.random())
@@ -28,4 +35,6 @@ print(math.pi)
 print('abc'.capitalize())
 
 print('abc'.center(12))
+
+print(','.join('hello'))
 
