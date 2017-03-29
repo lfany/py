ee5a43ab01a6e58db63b00149b6472cbfab5da37
hello <hello@ifnot.cc>
2017-03-29 10:37:30 +0800

    code

---
+++ hello/statement.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+if not True:
+    print("hello")
+else:
+    print("world")
+
+count = 0
+while count < 9:
+    count += 1
+    print(count)
+
+
+a = {'a', 'b', 'c'}
+
+for item in a:
+    print(item)
+
+import math
+for i in range(0, 100):
+    print(i)
+
+print(math.pi)
+
+# import random
+# print(random.random())
+
+print('abc'.capitalize())
+
+print('abc'.center(12))
+
