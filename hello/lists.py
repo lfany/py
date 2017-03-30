5a8b5d32566a3610c9347ad7287d4c593a612a76
hello <hello@ifnot.cc>
2017-03-30 13:36:35 +0800

    update delete add lists

--- hello/lists.py
+++ hello/lists.py
@@ -16,8 +16,23 @@ for i in range(len(lists)):
 
 ## add
 
+lists.append('new item')
+lists.append("zxc")
+print(lists)
+
 ## delete
+lists.remove(22.2)
+del lists[0]
+print(lists)
 
 ## update
+
+lists[0] = 2
+lists[1] = 2.2
+lists[2] = {1, 0.1, 'zxc'}
+lists[3] = (1, 2.1, 'xx')
+lists[4] = 'xxxxxx'
+
+print(lists)
 
 
