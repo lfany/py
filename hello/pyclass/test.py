076cbf2152219865f77946c0be9c14a525f273fb
hello <hello@ifnot.cc>
2017-03-31 20:44:23 +0800

    add test

---
+++ hello/pyclass/test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import hello.pyclass.emplyee as emplyee
+
+from hello.pyclass.emplyee import Emplyee
+
+ee = Emplyee("name", "sal")
+ee.display()
+
+
+e = emplyee.Emplyee("n", 's')
+e.display()
