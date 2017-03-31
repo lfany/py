66db97d386873da112a4076b8a36506fae449328
hello <hello@ifnot.cc>
2017-03-31 20:37:45 +0800

    class

---
+++ hello/pyclass/emplyee.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+class Emplyee:
+    'hello'
+    empCount = 0
+
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+        Emplyee.empCount += 1
+
+    def displayCount(self):
+        print('Total employee %d' % Emplyee.empCount)
+
+    def displayEmployee(self):
+        print('Name: %s, Salary: %s' % (self.name, self.salary))
+
+    def display(self):
+        print('Total: {}'.format(Emplyee.empCount))
+        print('Name: {}; Salary: {}'.format(self.name, self.salary))
+        print('Name: {0}; Salary: {1}'.format(self.name, self.salary))
+        print('Name: {name}; Salary: {salary}'.format(name=self.name, salary=self.salary))
+
+print("Employee.__doc__:", Emplyee.__doc__)
+print("Employee.__name__:", Emplyee.__name__)
+print("Employee.__module__:", Emplyee.__module__)
+print("Employee.__bases__:", Emplyee.__bases__)
+print("Employee.__dict__:", Emplyee.__dict__)
+
+# e = Emplyee('aa', 'xx')
+# e.displayCount()
+# e.displayEmployee()
+# e.display()
