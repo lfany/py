3441849147a70a7f02263ebc8bcd5f0cc1c665b1
hello <hello@ifnot.cc>
2017-04-13 18:34:26 +0800

    update

--- tests/spy.py
+++ tests/spy.py
@@ -2,6 +2,7 @@
 
 import urllib
 import  bs4
+import requests
 
 file = open('xx.html', 'w+', encoding='utf-8')
 
@@ -10,27 +11,30 @@ def get_content(page):
 
     base_url = 'http://www.qiushibaike.com/hot/page/' + str(page)
     print(base_url)
-    file.write('<a href="' + base_url + '" />\n')
+    file.write('<a href="' + base_url + '" > ' + base_url + ' </a><br />\n')
 
-    req = urllib.request.Request(base_url, data=None, headers={'User-Agent': 'xxx'},
-                             method='GET')
-    a = urllib.request.urlopen(req)
-    html = bs4.BeautifulSoup(a, 'html5lib')
+    # req = urllib.request.Request(base_url, data=None, headers={'User-Agent': 'xxx'},
+    #                          method='GET')
+    # a = urllib.request.urlopen(req)
+    a = requests.get(base_url, headers={'User-Agent': 'xxx'})
+    html = bs4.BeautifulSoup(a.text, 'html5lib')
 
 
-    a = html.findAll('div', attrs={'class': 'content'})
+    a = html.findAll('div', attrs={'class': 'article block untagged mb15'})
 
     count = 0
     for i in a:
         count += 1
         print('=================')
-        file.write('=================' + '\n')
+        file.write('=================' + '<br />\n')
+        print(i.h2.text)
+        file.write(i.h2.text + '<br />\n')
         print(i.span.text)
-        file.write(i.span.text + '\n')
+        file.write(i.span.text + '<br />\n')
         print('=================')
-        file.write('=================' + '\n')
+        file.write('=================' + '<br />\n')
     print(count)
-    file.write(str(count) + '\n')
+    file.write(str(count) + '<br />\n')
 
 
 for i in range(10):
