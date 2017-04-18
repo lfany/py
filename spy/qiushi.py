afe69d16df01d17f44ac3cecaf3c17e720e18fe1
hello <hello@ifnot.cc>
2017-04-18 17:11:29 +0800

    no print

--- spy/qiushi.py
+++ spy/qiushi.py
@@ -91,7 +91,7 @@ def get_page(page):
             lists.append(article)
         data = json.dumps(Data(lists), default=lambda o: o.__dict__, sort_keys=True, indent=4,
                           ensure_ascii=False)
-        print(data)
+        # print(data)
         return data
         # dumps = json.dumps(article, default=lambda o: o.__dict__, sort_keys=True, indent=4)
         # print(dumps)
