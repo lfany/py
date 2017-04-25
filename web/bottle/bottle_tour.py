1b217362e79c7d662d6a37a382e8e4c2c32f2902
hello <hello@ifnot.cc>
2017-04-25 15:20:25 +0800

    bottle update

--- web/bottle/bottle_tour.py
+++ web/bottle/bottle_tour.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-from bottle import Bottle, run
+from bottle import Bottle, run, static_file, request
 
 app = Bottle()
 
 
 @app.route('/hello/<name>')
-def index(name):
+def index(name='Stranger'):
     return 'hello world! {xx} '.format(xx=name)
 
 
@@ -18,6 +18,36 @@ def file_not_found(error):
 @app.route('/test/<param>')
 def test(param):
     return 'test %s' % param
+
+
+@app.route('/code')
+@app.route('/code/')
+def code():
+    return static_file(root='./', filename='myspider.py')
+
+
+@app.get('/login')  # 或 @route('/login')
+def login_form():
+    return '''<form method="POST" action="/login">
+<input name="name" type="text" />
+<input name="password" type="password" />
+<input type="submit" />
+</form>'''
+
+
+def check_login(name, password):
+    pass
+    return True
+
+
+@app.post('/login')  # 或 @route('/login', method='POST')
+def login_submit():
+    name = request.forms.get('name')
+    password = request.forms.get('password')
+    if check_login(name, password):
+        return "<p> 登录成功 </p>"
+    else:
+        return "<p> 登录失败 </p>"
 
 
 run(app=app, host='localhost', port=44, debug=True, reloader=True)
