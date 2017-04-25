72afc54a2e0453eff08d50f6785f0ebad614e40f
hello <hello@ifnot.cc>
2017-04-25 17:49:19 +0800

    fixes

--- spy/stack/stack/spiders/stack_spider.py
+++ spy/stack/stack/spiders/stack_spider.py
@@ -6,7 +6,9 @@ from scrapy import Selector
 class StackSpiderSpider(scrapy.Spider):
     name = "stack_spider"
     allowed_domains = ["stackoverflow.com"]
-    start_urls = ['http://stackoverflow.com/questions?pagesize=50&sort=newest']
+    start_urls = [
+        'http://stackoverflow.com/questions?pagesize=50&sort=newest'
+    ]
 
     def parse(self, response):
         questions = Selector(response).xpath('//div[@class="summary"]/h3')
