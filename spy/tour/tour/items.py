7e7679492f43ea01cb1863f13d636de6090cc3d9
hello <hello@ifnot.cc>
2017-05-05 17:41:16 +0800

    baidu search

--- spy/tour/tour/items.py
+++ spy/tour/tour/items.py
@@ -11,4 +11,5 @@ import scrapy
 class TourItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    title = scrapy.Field()
+    url = scrapy.Field()
--- spy/tour/tour/spiders/tourspy.py
+++ spy/tour/tour/spiders/tourspy.py
@@ -4,8 +4,29 @@ import scrapy
 
 class TourspySpider(scrapy.Spider):
     name = "tourspy"
+    params = 's?wd='
+    keyword = '你好'
     allowed_domains = ["www.baidu.com"]
-    start_urls = ['http://www.baidu.com/']
+    start_urls = ['http://www.baidu.com/' + params + keyword]
 
     def parse(self, response):
-        self.logger.info("%s received", response.url)
+        # self.logger.info("%s received", response.url)
+        # print(response.body.decode('utf-8'))
+        for i in response.xpath('//div[@class="result c-container "]'):
+            print(i.xpath('h3/a/@href').extract_first())
+            words = i.xpath('h3/a/text()').extract()
+            words.insert(1, i.xpath('h3/a/em/text()').extract_first())
+            print('##'.join(words))
+
+            from spy.tour.tour.items import TourItem
+            item = TourItem()
+            item['title'] = '##'.join(words).encode('utf-8').decode('unicode_escape')
+            item['url'] = i.xpath('h3/a/@href').extract_first()
+            yield item  # {
+            #     'title': '##'.join(words),
+            #     'url': i.xpath('h3/a/@href').extract_first(),
+            # }
+
+            # i.xpath('h3/a/em/text()').extract_first(),
+            # i.xpath('h3/a/text()').extract())
+            # print(i.xpath('h3/a').extract_first())
