64fa2e64a293bcc2a8be58a7b946983ac83834ca
hello <hello@ifnot.cc>
2017-07-14 11:58:59 +0800

    add more model

--- web/dj/Model/admin.py
+++ web/dj/Model/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from .models import TestModel
+from .models import TestModel, Contact, Tag
 
 # Register your models here.
 
-admin.site.register(TestModel)
+admin.site.register([TestModel, Contact, Tag])
