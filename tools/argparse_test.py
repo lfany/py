9a6b590c2697c35c50bd66857ce6d7a8e2adfa76
hello <hello@ifnot.cc>
2017-07-20 14:52:38 +0800

    update

--- tools/argparse_test.py
+++ tools/argparse_test.py
@@ -4,22 +4,39 @@ import argparse
 import optparse
 import getopt
 
-from optparse import OptionParser
+# from optparse import OptionParser
+#
+# import sys
+#
+# parser = OptionParser()
+# parser.add_option("-f", "--file", dest="filename",
+#                   help="write report to FILE", metavar="FILE")
+# parser.add_option("-q", "--quiet",
+#                   action="store_false", dest="verbose", default=True,
+#                   help="don't print status messages to stdout")
+# parser.add_option("-x", "--x")
+#
+# (options, args) = parser.parse_args()
+#
+# if not len(args):
+#     parser.print_help()
+#     print(sys.argv)
+#     print(options)
+#     print(args)
 
-import sys
 
-parser = OptionParser()
-parser.add_option("-f", "--file", dest="filename",
-                  help="write report to FILE", metavar="FILE")
-parser.add_option("-q", "--quiet",
-                  action="store_false", dest="verbose", default=True,
-                  help="don't print status messages to stdout")
-parser.add_option("-x", "--x")
+import argparse
 
-(options, args) = parser.parse_args()
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('integers', metavar='N', type=int, nargs='+',
+                    help='an integer for the accumulator')
+parser.add_argument('--sum', dest='accumulate', action='store_const',
+                    const=sum, default=max,
+                    help='sum the integers (default: find the max)')
 
-if not len(args):
-    parser.print_help()
-    print(sys.argv)
-    print(options)
-    print(args)
+# args = parser.parse_args()
+
+args = parser.parse_args()
+print(args.accumulate(args.integers))
+parser.print_help()
+
